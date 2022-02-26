c6b9c77bbe98d16420f770f250fd9fb03a768f96
Akhil <akhilprem90@gmail.com>
2022-02-26 08:51:31 +0530

    home page, product page and product details page added

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Navbar, Nav, Container, Row, NavDropdown } from "react-bootstrap";
+
+function Header() {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand>ProShop</Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link>
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+
+              <Nav.Link>
+                <i className="fas fa-user"></i>Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
